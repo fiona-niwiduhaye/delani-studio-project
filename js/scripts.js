4d6e9cbd6af4be5b1d426918dc779a89ca2f2ca9
fiona <fionaniwe@gmail.com>
2020-10-04 18:40:32 +0200

    popup function is added

--- js/scripts.js
+++ js/scripts.js
@@ -84,18 +84,18 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  $("form#form34A").submit(function(event){
+  $("form#form1").submit(function(event){
     // event.preventDefault();
-    var name = $("input#MERGE1").val();
-    var email = $("input#MERGE0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-      alert (name + ", we have received your message. Thank you for reaching out to us.");
-    }
-    else {
-      alert("Please enter your name and email!");
+  var name = $("input#name").val();
+  var email = $("input#email").val();
+  var message = $("textarea#message").val();
+
+  if($("input#name").val() && $("input#email").val()){
+      
+      alert ("Hello "+ name + ",  Thank you for reaching out to us.");
+  
     }
     
-  });
+});
 
 });
